53ac84f47a43661f4af5a35cad3585651e3038ae
aseelkhaseeb1 <aseelkhaseeb79@gmail.com>
2023-05-06 17:40:11 +0300

    Lab1

---
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.lab1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText editText=(EditText)findViewById(R.id.editText);
+                TextView textView=(TextView) findViewById(R.id.textView);
+                textView.setText(editText.getText().toString());
+            }
+        });
+    }
+}
